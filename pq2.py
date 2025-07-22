ea88e530f9ef07de64735589b9e34b961266b73f
parika-ezlearn <parikaj@ezlearn.in>
2025-07-22 23:24:16 +0530

    done q2

---
+++ pq2.py
@@ -0,0 +1,8 @@
+'''Python Program to Swap Two Elements in a List'''
+a = [10, 20, 30, 40, 50]
+
+# Swapping elements at index 0 and 4
+# using multiple assignment
+a[0], a[4] = a[4], a[0]
+
+print(a)
