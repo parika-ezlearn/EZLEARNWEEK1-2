c4ab79fa3a62cbb06756499a68c283140b8a46ed
parika-ezlearn <parikaj@ezlearn.in>
2025-07-23 12:51:53 +0530

    done q20

---
+++ pq20.py
@@ -0,0 +1,16 @@
+'''Python program to read file word by word
+'''
+# Python program to read 
+# file word by word
+ 
+# opening the text file
+with open('GFG.txt','r') as file:
+ 
+    # reading each line    
+    for line in file:
+ 
+        # reading each word        
+        for word in line.split():
+ 
+            # displaying the words           
+            print(word)
