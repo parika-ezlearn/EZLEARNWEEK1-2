554df32e4de13f47beef3287f5d4833e89c0b71a
parika-ezlearn <parikaj@ezlearn.in>
2025-07-23 22:39:28 +0530

    done q39

---
+++ pq39.py
@@ -0,0 +1,9 @@
+'''2. Using sorted() function
+'''
+s = {5, 3, 9, 1, 7}
+
+# Sorting the set (converts it into a sorted list)
+sorted_s = sorted(s)
+
+print("Minimum element: ", sorted_s[0])
+print("Maximum element: ", sorted_s[-1])
